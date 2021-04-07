6ec4f4ce73b2aeeefdc0d569d1e7a4ebe1d20ae0
Dmitriy Vasin <vasin.mitya@gmail.com>
2021-04-08 01:24:22 +0300

    Выполнил домашнее задание №2

--- src/com/company/homework2/Main.java
+++ src/com/company/homework2/Main.java
@@ -1,6 +1,10 @@
 package com.company.homework2;
 
+import java.util.Arrays;
+
 public class Main {
+    public static final int COLUMN = 4;
+    public static final int ROW = 4;
     public static void main(String[] args) throws MyArraySizeException {
         String[][] array_error1 = {{"1", "2", "3", "4"},
                 {"5", "6", "7", "8"},
@@ -30,9 +34,10 @@ public class Main {
 
     public static void arr(String[][] array) throws MyArraySizeException, MyArrayDataException{
         int sum = 0;
-        if(array.length!=4) throw new MyArraySizeException();
+        if(array.length!=ROW) throw new MyArraySizeException();
         for (int i = 0; i < array.length; i++) {
-            if(array[i].length!=4) throw  new MyArraySizeException();
+            if(array[i].length!=COLUMN) throw  new MyArraySizeException();
+            System.out.println(Arrays.deepToString(array));
             for(int j = 0; j < array[i].length; j++){
                 try {
                     sum = sum + Integer.parseInt(array[i][j]);
